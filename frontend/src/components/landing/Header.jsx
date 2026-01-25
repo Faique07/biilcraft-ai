2da8d0b7e55548447acb60f2a8d93ff2319214de
Faique07 <faique1202@gmail.com>
2026-01-25 14:04:30 +0530

    Initial commit

---
+++ frontend/src/components/landing/Header.jsx
@@ -0,0 +1,162 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { FileText, Menu, X } from "lucide-react";
+import ProfieDropdown from "../layout/ProfieDropdown";
+import { useNavigate } from "react-router-dom";
+import Button from "../ui/Button";
+
+
+
+const Header = () => {
+  
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const isAuthenticated = false;
+  const user = { name: "Faique", email: "faique1202@gmail.com" };
+  const logout = () => {};
+
+  const navigate = useNavigate();
+
+  const [profileDropDownOpen, setProfileDropDownOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 10);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <header
+      className={`fixed top-0 w-full z-50 transition-all duration-300 bg-gray-100 ${isScrolled ? "bg-white/95 backdrop-blur-sm shadow-lg" : "bg-white/0"}`}
+    >
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16 lg:h-20">
+          <div className="flex items-center space-x-2">
+            <div className="w-8 h-8 bg-[#670229] rounded-md flex items-center justify-center">
+              <FileText className="text-white w-4 h-4" />
+            </div>
+            <span className="text-xl font-bold text-gray-900">Bill Craft</span>
+          </div>
+          <div className="hidden lg:flex lg:items-center lg:space-x-8">
+            <a
+              href="#features"
+              className="text-gray-600 hover:text-gray-900 font-medium transition-colors duration-200"
+            >
+              Features
+            </a>
+            <a
+              href="#testimonials"
+              className="text-gray-600 hover:text-gray-900 font-medium transition-colors duration-200"
+            >
+              Testimonials
+            </a>
+            <a
+              href="#faq"
+              className="text-gray-600 hover:text-gray-900 font-medium transition-colors duration-200"
+            >
+              FAQs
+            </a>
+          </div>
+          <div className="hidden lg:flex items-center space-x-4">
+            {isAuthenticated ? (
+              <ProfieDropdown
+                isOpen = {profileDropDownOpen}
+                onToggle = {(e)=>{
+                    e.stopPropagation();
+                    setProfileDropDownOpen(!profileDropDownOpen)
+                }}
+                avatar = {user?.avatar || ""}
+                companyName = {user?.name || ""}
+                email = {user?.email || ""}
+                onLogout = {logout}
+              />
+            ) : (
+              <>
+                <Link
+                  to="/login"
+                  className="text-black hover:text-gray-900 font-medium transition-colors duration-200"
+                >
+                  Login
+                </Link>
+
+                <Link
+                  to="/signup"
+                  className="bg-[#670229] hover:bg-[#850335] px-6 py-2.5 text-white rounded-lg font-medium transition-all duration-200"
+                >
+                  SignUp
+                </Link>
+              </>
+            )}
+          </div>
+          <div className="lg:hidden">
+            <button
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+              className="p-2 rounded-lg text-gray-600 hover:text-gray-900 hover:bg-gray-100 transition-colors duration-200"
+            >
+              {isMenuOpen ? (
+                <X className="w-6 h-6" />
+              ) : (
+                <Menu className="w-6 h-6" />
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+      {isMenuOpen && (
+        <div className="lg:hidden absolute top-full left-0 right-0 bg-white border-b border-gray-200 shadow-lg">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <a
+              href="#features"
+              className="block px-4 py-3 text-gray-600 hover:text-gray-900 hover:bg-gray-50 font-medium transition-colors duration-200"
+            >
+             Features
+            </a>
+            <a
+              href="#testimonials"
+              className="block px-4 py-3 text-gray-600 hover:text-gray-900 hover:bg-gray-50 font-medium transition-colors duration-200"
+            >
+            Testimonials
+            </a>
+            <a
+              href="#faq"
+              className="block px-4 py-3 text-gray-600 hover:text-gray-900 hover:bg-gray-50 font-medium transition-colors duration-200"
+            >
+              FAQ
+            </a>
+            <div className=""></div>
+            {isAuthenticated ? (
+              <div className="border-t border-gray-200 my-2">
+                <Button
+                  onClick={()=>navigate('/dashboard')}
+                  className="w-full"
+                >
+                  Go to Dashboard
+                </Button>
+              </div>
+            ) : (
+              <>
+                <Link
+                  to="/login"
+                  className="block px-4 py-3 text-gray-600 hover:text-gray-900 hover:bg-gray-50 font-medium transition-colors duration-200"
+                >
+                  Login
+                </Link>
+                <Link
+                  to="/signup"
+                  className="block w-full text-left bg-gray-900 hover:bg-gray-800 text-white px-4 py-3 rounded-lg font-medium transition-all duration-200"
+                >
+                  SignUp
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      )}
+    </header>
+  );
+};
+
+export default Header;
